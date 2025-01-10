db7a3614fed9f39d0222a8bb6c27d14215df3720
ManuelBarindelli <manubarinde@gmail.com>
2025-01-10 08:27:30 +0000

    modifiche

--- flight-monitor/get_flights.php
+++
@@ -1,9 +0,0 @@
-<?php
-// get_flights.php
-require 'db.php';
-
-$stmt = $pdo->query("SELECT * FROM flights WHERE departure_time >= NOW()");
-$flights = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($flights);
-?>
